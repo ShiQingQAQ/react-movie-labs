73fa3bfcafa69a034c38116a933959e0cee4bf23
ShiQingQAQ <20108872@mail.wit.ie>
2024-11-02 20:05:19 +0000

    Added new review support.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -9,7 +9,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
-import React, {useState, useEffect}  from "react";
+import React from "react";
 import { getGenres } from "../../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from '../spinner'
